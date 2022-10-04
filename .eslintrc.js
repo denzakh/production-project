06b9f917c43ab45e1a9d0876e063f3494c4bc46b
Denis Zakharchenko <den.zakh@gmail.com>
2022-10-04 12:26:46 +0300

    jest install 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
     "react/jsx-props-no-spreading": "warn",
     "react/react-in-jsx-scope": "off",
     "react/require-default-props": "off",
-    "i18next/no-literal-string": ["error", { markupOnly: true, ignoreAttribute: ['to']}]
+    "i18next/no-literal-string": ["error", { markupOnly: true, ignoreAttribute: ["to", "data-testid"]}]
   },
   globals: {
     __IS_DEV__: true,
