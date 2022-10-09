4a168419bb84bea83b0c0528ddfecf7213a25026
Denis Zakharchenko <den.zakh@gmail.com>
2022-10-09 17:40:23 +0300

    25 Storybook

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,7 +4,7 @@ import cls from "./Button.module.scss";
 
 export enum ThemeButton {
   CLEAR = "clear",
-  BTN = "btn",
+  OUTLINE = "outline",
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
