abef69c49ddd8c2181305baefd72f20ce3f7e496
Denis Zakharchenko <den.zakh@gmail.com>
2022-10-27 01:42:15 +0300

    34 Авторизация

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,6 @@ module.exports = {
         'no-shadow': 'off',
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
-        'linebreak-style': 0,
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': [
             'error',
@@ -50,7 +49,7 @@ module.exports = {
                 ignoreAttribute: ['data-testid', 'to'],
             },
         ],
-        'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'max-len': ['error', { ignoreComments: true, code: 120 }],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
