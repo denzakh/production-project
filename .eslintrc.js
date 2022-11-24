0bed5372d619ce4ad5ae6ca013940a785d015980
Denis Zakharchenko <den.zakh@gmail.com>
2022-11-24 20:34:46 +0300

    Фильтры. Сортировка. Поиск. Tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'off',
+        'operator-linebreak': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
