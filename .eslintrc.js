f0f7d84d349192a76eb6ad2f024a2924662f0a1f
Denis Zakharchenko <den.zakh@gmail.com>
2022-12-08 02:40:35 +0300

    Notification

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap', 'as'],
+                ignoreAttribute: ['role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap', 'as', 'border', 'height'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 130 }],
