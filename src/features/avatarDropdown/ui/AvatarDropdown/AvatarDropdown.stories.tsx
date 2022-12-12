479d9020b2b425383b0a556332ad3b7deaf0b0c0
Denis Zakharchenko <den.zakh@gmail.com>
2022-12-12 15:40:28 +0300

    fix storybook

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import withMock from 'storybook-addon-mock';
 import { AvatarDropdown } from './AvatarDropdown';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { UserRole } from '@/entities/User/model/consts/userConsts';
 
 export default {
-    title: 'shared/AvatarDropdown',
+    title: 'features/AvatarDropdown',
     component: AvatarDropdown,
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [withMock],
 } as ComponentMeta<typeof AvatarDropdown>;
 
-const Template: ComponentStory<typeof AvatarDropdown> = (args) => <AvatarDropdown {...args} />;
+const Template: ComponentStory<typeof AvatarDropdown> = (args) => (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <AvatarDropdown {...args} />
+);
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.decorators = [
+    StoreDecorator({
+        user: {
+            authData: {
+                id: '1',
+                username: 'Randomusers',
+                avatar: 'http://xsgames.co/randomusers/avatar.php?g=male',
+                roles: [UserRole.ADMIN, UserRole.MANAGER],
+            },
+        },
+    }),
+];
