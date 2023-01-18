133e4d22094e80f505e01bad54fe24d6b56f6846
Denis Zakharchenko <den.zakh@gmail.com>
2023-01-18 21:46:13 +0300

    компоненты с Cypress

--- cypress/support/commands/article.ts
+++ cypress/support/commands/article.ts
@@ -14,18 +14,22 @@ const defaultArticle = {
     blocks: [],
 };
 
-export const createArticle = (article?: Article) => cy.request({
-    method: 'POST',
-    url: 'http://localhost:8000/articles',
-    headers: { Authorization: 'asasf' },
-    body: article ?? defaultArticle,
-}).then((resp) => resp.body);
+export const createArticle = (article?: Article) => {
+    return cy.request({
+        method: 'POST',
+        url: 'http://localhost:8000/articles',
+        headers: { Authorization: 'asasf' },
+        body: article ?? defaultArticle,
+    }).then((resp) => resp.body);
+};
 
-export const removeArticle = (articleId: string) => cy.request({
-    method: 'DELETE',
-    url: `http://localhost:8000/articles/${articleId}`,
-    headers: { Authorization: 'asasf' },
-});
+export const removeArticle = (articleId: string) => {
+    return cy.request({
+        method: 'DELETE',
+        url: `http://localhost:8000/articles/${articleId}`,
+        headers: { Authorization: 'asasf' },
+    });
+};
 
 declare global {
     namespace Cypress {
